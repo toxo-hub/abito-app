10677524cfda800c7da3a46b3e0c5ade286bd955
toxo-hub <azelydkov@gmail.com>
2025-03-28 20:56:06 +0300

    Removing debug information from a ErrorPage component

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,6 @@
-import { useRouteError } from 'react-router-dom';
 import { NotFound } from '../components/NotFound/NotFound';
 
 export const ErrorPage = () => {
-    const error = useRouteError();
-    console.log(error);
     return (
         <NotFound/>
     )
